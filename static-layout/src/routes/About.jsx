81504bcfbcf8d8288c0f75684c44058e5d55dcfe
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-19 20:14:37 -0800

    Add about page content

--- static-layout/src/routes/About.jsx
+++ static-layout/src/routes/About.jsx
@@ -1,5 +1,44 @@
 import React from "react"
+import { Flex, Box } from "rebass"
 
 export const About = () => {
-  return <div>About</div>
+  return (
+    <Flex>
+      <Box pr={4}>
+        <p>
+          Unseen Worlds is a record label releasing quality editions of
+          unheralded and revolutionary, yet accessible, avant garde music. We
+          are interested in media that capture a timeless ecstasy of creativity,
+          that seem out of space, alien, yet are deeply resonant and
+          approachable. They are both primary and mysterious. They contain
+          multitudes, engaging the listener through their historical context and
+          sheer musicality. Our releases are surprisingly disparate and
+          reliable.
+        </p>
+
+        <p>You may get in touch with us at unseenworlds@unseenworlds.com</p>
+
+        <p>You can also connect with us on Facebook, Twitter, and Instagram</p>
+
+        <p>
+          Unseen Worlds <br />
+          PO Box 250558 <br />
+          New York, NY 10025
+        </p>
+      </Box>
+      <Box>
+        <p>Our titles see distribution worldwide via Secretly Distribution</p>
+
+        <p>
+          Direct Retail Sales: <br />
+          domestic@secretlydistribution.com
+        </p>
+
+        <p>
+          International Sales: <br />
+          international@secretlydistribution.com
+        </p>
+      </Box>
+    </Flex>
+  )
 }
